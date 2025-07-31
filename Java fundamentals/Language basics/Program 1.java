359abeb04c487d193030a0de784eb31f144951c4
MONISHA K <k48172473@gmail.com>
2025-07-31 13:47:46 +0530

    Program 1.java

--- Java fundamentals/Language basics/Program 1.java
+++ Java fundamentals/Language basics/Program 1.java
@@ -1,1 +1,12 @@
-
+public class program 1{
+  public static void main(String args[])
+  {
+    if(args.length !=2){
+      System.out.println("Two arguments required");
+      return;
+    }
+    String s1=args[0];
+    String s2=args[1];
+    System.out.println(s1+" technologies "+ s2);
+  }
+}
