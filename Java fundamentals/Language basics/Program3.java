1041e5caf24738eb9baa9e24fae6ab1b9cda3474
MONISHA K <k48172473@gmail.com>
2025-07-31 14:07:44 +0530

    Program3.java

--- Java fundamentals/Language basics/Program3.java
+++ Java fundamentals/Language basics/Program3.java
@@ -1,1 +1,15 @@
-
+public class program3
+  {
+    public static void main(String args[])
+    {
+      if(args.length!=2)
+      {
+        System.out.println("Two arguments are required");
+        return;
+      }
+      int a=Integer.parseInt(args[0]);
+      int b=Integer.parseInt(args[1]);
+      int c=a+b;
+      System.out.println("The sum of "+a+" "+"and "+b+" "+"is "+c);
+    }
+  }
