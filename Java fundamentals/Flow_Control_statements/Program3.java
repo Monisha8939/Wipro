32326c4f5af4f36da6ed88771b9a002b5ddfc9e5
MONISHA K <k48172473@gmail.com>
2025-08-01 13:18:51 +0530

    Program3.java

--- Java fundamentals/Flow_Control_statements/Program3.java
+++ Java fundamentals/Flow_Control_statements/Program3.java
@@ -1,1 +1,12 @@
-
+public class program3
+  {
+    public static void main(String args[]){
+      int a=Integer.parseInt(args[0]);
+      if(a%2==0){
+        System.out.println("Even");
+      }
+      else{
+        System.out.println("Odd");
+      }
+    }
+  }
