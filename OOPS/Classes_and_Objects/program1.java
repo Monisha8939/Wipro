f2a8f52fc4f46b5d78afda39f52e5a8116f10400
MONISHA K <k48172473@gmail.com>
2025-08-02 12:17:21 +0530

    program1.java

---
+++ OOPS/Classes_and_Objects/program1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Box {
+    double w, h,b;
+    Box(double width, double height, double breadth) 
+  {
+         w=width;
+         h=height;
+         b=breadth;
+    }
+   public static double volume() 
+  {
+        return  w*h*b;
+    }
+}
+
+ public class Main
+  {
+    public static void main(String[] args)
+  {
+        Scanner sc=new Scanner(System.in);
+        double w=sc.nextDouble();
+        double h=sc.nextDouble();
+        double b=sc.nextDouble();
+        Box box=new Box(w,h,b);
+        System.out.println(box.volume());
+        sc.close();
+   }
+}
