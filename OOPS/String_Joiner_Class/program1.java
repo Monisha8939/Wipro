180fb661c5f8e50aa818ddb1da3a84a0b2f14853
MONISHA K <k48172473@gmail.com>
2025-08-04 14:04:45 +0530

    program1.java

--- OOPS/String_Joiner_Class/program1.java
+++ OOPS/String_Joiner_Class/program1.java
@@ -1,1 +1,20 @@
-
+import java.util.Scanner;
+public class program1
+  {
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      ArrayList<String>arr=new ArrayList<>();
+      for(int i=0;i<n;i++)
+        {
+          arr.add(sc.nextLine());
+        }
+       StringJoiner sj = new StringJoiner(",", "{", "}");
+        for (String name : arr) {
+            sj.add(name);
+        }
+      System.out.println(sj.toString());
+      sc.close();
+    }
+}
