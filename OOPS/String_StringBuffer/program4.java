0388de83fab7663479a32ec667cb6563a011ae97
MONISHA K <k48172473@gmail.com>
2025-08-07 23:28:22 +0530

    program4.java

---
+++ OOPS/String_StringBuffer/program4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class program4
+  {
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        int len = str.length();
+        if (len % 2 == 0)
+        {
+            String ans= str.substring(0, len/2);
+            System.out.println(ans);
+        } 
+        else
+        {
+            System.out.println("null");
+        }
+        sc.close();
+    }
+}
