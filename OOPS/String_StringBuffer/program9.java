a446a7a52c6415edcda9b559255056096afd3803
MONISHA K <k48172473@gmail.com>
2025-08-19 22:18:44 +0530

    program9.java

--- OOPS/String_StringBuffer/program9.java
+++ OOPS/String_StringBuffer/program9.java
@@ -1,1 +1,25 @@
-
+import java.util.Scanner;
+public class program9
+  {
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String b=sc.nextLine();
+        StringBuilder res=new StringBuilder();
+        int maxlen=Math.max(a.length(),b.length());
+       for (int i=0;i<maxlen;i++)
+         {
+            if (i<a.length())
+            {
+                res.append(a.charAt(i));
+            }
+            if (i < b.length()) 
+            {
+                res.append(b.charAt(i));
+            }
+        }
+      System.out.println(res.toString());
+      sc.close();
+    }
+}
