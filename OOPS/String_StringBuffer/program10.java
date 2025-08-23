6847d92e63dfd035c2da6c3e87746bdd34a0827c
MONISHA K <k48172473@gmail.com>
2025-08-23 22:40:28 +0530

    program10.java

--- OOPS/String_StringBuffer/program10.java
+++ OOPS/String_StringBuffer/program10.java
@@ -1,1 +1,17 @@
-
+import java.util.Scanner;
+public class program10
+  {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        int n=sc.nextInt();
+        String s= str.substring(str.length()-n);
+        StringBuilder res=new StringBuilder();
+       for (int i = 0; i < n; i++) {
+            res.append(s);
+        }
+       System.out.println(res.toString());
+       sc.close();
+    }
+}
