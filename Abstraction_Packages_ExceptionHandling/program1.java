15f335643789250897bfe959b65eda802f26a1af
MONISHA K <k48172473@gmail.com>
2025-08-25 23:18:42 +0530

    program1.java

--- Abstraction_Packages_ExceptionHandling/program1.java
+++ Abstraction_Packages_ExceptionHandling/program1.java
@@ -1,1 +1,64 @@
-
+abstract class Compartment 
+{
+    public abstract String notice();
+}
+class FirstClass extends Compartment 
+{
+    public String notice() 
+   {
+        return "First Class Compartment";
+    }
+}
+class Ladies extends Compartment 
+{
+    public String notice() 
+   {
+        return "Ladies Compartment";
+    }
+}
+class General extends Compartment
+  {
+    public String notice()
+    {
+        return "General Compartment";
+    }
+}
+class Luggage extends Compartment 
+{
+    public String notice() 
+  {
+        return "Luggage Compartment";
+    }
+}
+import java.util.Random;
+public class TestCompartment 
+{
+    public static void main(String[] args) 
+  {
+        Compartment[] compartments=new Compartment[10];
+        Random rand=new Random();
+        for (int i=0;i<compartments.length;i++)
+          {
+            int r=rand.nextInt(4)+1;  
+            switch(r) 
+            {
+                case 1:
+                    compartments[i]=new FirstClass();
+                    break;
+                case 2:
+                    compartments[i]=new Ladies();
+                    break;
+                case 3:
+                    compartments[i]=new General();
+                    break;
+                case 4:
+                    compartments[i]=new Luggage();
+                    break;
+            }
+        }
+        for (int i=0;i<compartments.length;i++)
+          {
+            System.out.println("Compartment"+(i+1)+":"+compartments[i].notice());
+        }
+    }
+}
