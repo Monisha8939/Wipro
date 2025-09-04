1438ac120994c3058397799a3d8fabaf1d607856
MONISHA K <k48172473@gmail.com>
2025-09-04 21:51:26 +0530

    program4.java

--- Abstraction_Packages_ExceptionHandling/Exception_Handling/program4.java
+++ Abstraction_Packages_ExceptionHandling/Exception_Handling/program4.java
@@ -1,1 +1,38 @@
+class InvalidCountryException extends Exception
+  {
+    public InvalidCountryException(String message) 
+    {
+        super(message);
+    }
+}
+public class  program4
+  {
+    public void registerUser(String username, String userCountry) throws InvalidCountryException 
+    {
+        if (!userCountry.equalsIgnoreCase("India")) 
+        {
+            throw new InvalidCountryException("User Outside India cannot be registered");
+        } else
+        {
+            System.out.println("User registration done successfully");
+        }
+    }
 
+    public static void main(String[] args)
+    {
+        UserRegistration userReg = new UserRegistration();
+        try {
+            userReg.registerUser("Mickey", "US");
+        } catch (InvalidCountryException e) 
+          {
+            System.out.println("InvalidCountryException:"+e.getMessage());
+        }
+      try 
+        {
+            userReg.registerUser("Mini", "India");
+        } catch (InvalidCountryException e) 
+        {
+            System.out.println("InvalidCountryException:"+e.getMessage());
+        }
+    }
+}
